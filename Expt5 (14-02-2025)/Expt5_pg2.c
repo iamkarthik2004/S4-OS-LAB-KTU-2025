781fe5e717b5a50159ce46580f6873149c4a6f55
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-02-16 15:05:28 +0530

    Update Expt5_pg2.c

--- Expt5 (14-02-2025)/Expt5_pg2.c
+++ Expt5 (14-02-2025)/Expt5_pg2.c
@@ -9,70 +9,71 @@ MEMORY ALLOCATION METHODS
 */
 
 #include <stdio.h>
-int main() 
-{
-	int nb, np, i, j;
 
+int main() {
+    int nb, np, i, j;
 
-	printf("Enter the number of memory blocks: ");
-	scanf("%d", &nb);
-	
-	int blockSize[nb], allocation[nb];
-	for (i = 0; i < nb; i++) {
-	printf("Enter size of block %d: ", i + 1);
-	scanf("%d", &blockSize[i]);
-	allocation[i] = -1;
-}
+    // Input number of memory blocks
+    printf("Enter the number of memory blocks: ");
+    scanf("%d", &nb);
+    
+    int blockSize[nb], allocation[nb];
 
+    // Input block sizes and initialize allocation array
+    for (i = 0; i < nb; i++) {
+        printf("Enter size of block %d: ", i + 1);
+        scanf("%d", &blockSize[i]);
+        allocation[i] = -1; // Initialize all blocks as unallocated
+    }
 
-	printf("Enter the number of processes: ");
-	scanf("%d", &np);
+    // Input number of processes
+    printf("Enter the number of processes: ");
+    scanf("%d", &np);
+    
+    int processSize[np];
 
-	int processSize[np];
-	for (i = 0; i < np; i++) 
-	{
-		printf("Enter size of process %d: ", i + 1);
-		scanf("%d", &processSize[i]);
-	}
-	
-	//BEST FIT
-	 for (j = 0; j < nb; j++) {
+    // Input process sizes
+    for (i = 0; i < np; i++) {
+        printf("Enter size of process %d: ", i + 1);
+        scanf("%d", &processSize[i]);
+    }
+
+    // Best Fit Allocation
+    for (i = 0; i < np; i++) {
+        int bestIndex = -1;
+        
+        for (j = 0; j < nb; j++) {
             if (blockSize[j] >= processSize[i] && allocation[j] == -1) {
-                int diff = blockSize[j] - processSize[i];
-                if (diff < minDiff) {
-                    minDiff = diff;
-                    bestIdx = j;
+                if (bestIndex == -1 || blockSize[j] < blockSize[bestIndex]) {
+                    bestIndex = j; // Select the best (smallest fitting) block
                 }
             }
         }
 
-
-        if (bestIdx != -1) {
-            allocation[bestIdx] = i;
+        // Allocate process if a suitable block is found
+        if (bestIndex != -1) {
+            allocation[bestIndex] = i;
         }
     }
 
+    // Display allocation details
+    printf("\nProcess No.\tProcess Size\tBlock No.\n");
+    for (i = 0; i < np; i++) {
+        printf("%d\t\t%d\t\t", i + 1, processSize[i]);
+        int allocated = 0;
+        
+        for (j = 0; j < nb; j++) {
+            if (allocation[j] == i) {
+                printf("B%d\n", j + 1);
+                allocated = 1;
+                break;
+            }
+        }
+        
+        if (!allocated) {
+            printf("Not Allocated\n");
+        }
+    }
 
-
-	printf("\nProcess No.\tProcess Size\tBlock No.\n");
-	for (i = 0; i < np; i++) 
-	{
-		printf("%d\t\t%d\t\t", i + 1, processSize[i]);
-		int allocated = 0;
-		for (j = 0; j < nb; j++) 
-		{
-			if (allocation[j] == i) 
-			{
-				printf("%d\n", j + 1);
-				allocated = 1;
-				break;
-			}
-		}
-		if (!allocated) 
-		{
-			printf("Not Allocated\n");
-		}
-	}
-
-return 0;
+    return 0;
 }
