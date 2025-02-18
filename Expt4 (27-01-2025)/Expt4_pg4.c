766deb08d6a71540704ef1e2703b5dae4001971f
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-02-18 19:15:41 +0530

    Update Expt4_pg4.c

--- Expt4 (27-01-2025)/Expt4_pg4.c
+++ Expt4 (27-01-2025)/Expt4_pg4.c
@@ -2,11 +2,70 @@
 21-01-2025
 Experiment No: 4
 Scheduling Algorithm
-(A) RR - Round Robin*/
+(D) Round Robin*/
 
-#include <stdio.h>
+#include<stdio.h>
+void roundRobin(int processes[],int n,int BT[],int quantum){
+	int rem_bt[n],wt[n],tat[n];
+	for(int i=0;i<n;i++){
+		rem_bt[i]=BT[i];
+	}
+	int t=0;
+	printf("\nGantt Chart\n");
+	printf("0");
+	while(1){
+		int done=1;
+		for(int i=0;i<n;i++){
+			if (rem_bt[i] > 0) {
+				done=0;
+				if(rem_bt[i]>quantum){
+					t+=quantum;
+					rem_bt[i]-=quantum;
+					printf("| P%d | %d",processes[i],t);
+				}
+				else{
+					t+=rem_bt[i];
+					wt[i]=t-BT[i];
+					rem_bt[i]=0;
+					printf(" | P%d | %d",processes[i],t);
+					}
+				}
+			}
+		
+	if(done==1)
+		break;
+	}
+	printf("\n");
+	for(int i=0;i<n;i++){
+		tat[i]=BT[i]+wt[i];
+	}
+	 printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+	 for (int i = 0; i < n; i++)
+        printf("P%d\t%d\t\t%d\t\t%d\n", processes[i], BT[i], wt[i], tat[i]);
 
-int main() {
-  
-    return 0;
+    float total_wt = 0, total_tat = 0;
+	 for(int i=0;i<n;i++){
+	 	total_wt+=wt[i];
+	 	total_tat+=tat[i];
+	 }
+	 printf("\n Average Waiting time: %.2f",total_wt/n);
+	 printf("\n Average Turnaround time: %.2f",total_tat/n);
+	
+}
+int main(){
+	int n,quantum;
+	printf("Enter the number of processes: ");
+	scanf("%d",&n);
+	int processes[n],BT[n];
+	printf("Enter the burst time of each process: \n");
+	for(int i=0;i<n;i++){
+		processes[i]=i+1;
+		printf("P%d: ",i+1);
+		scanf("%d",&BT[i]);
+		
+	}
+	printf("Enter the time quantum: ");
+	scanf("%d",&quantum);
+	roundRobin(processes,n,BT,quantum);
+	return 0;
 }
