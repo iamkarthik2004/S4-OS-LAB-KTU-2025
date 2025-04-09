8191e0f3d085bf9eb3238a3d1621b8ad2e647bcc
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-04-09 23:10:37 +0530

    Update C-SCAN.c

--- Expt10 (03-04-2025)/C-SCAN.c
+++ Expt10 (03-04-2025)/C-SCAN.c
@@ -1,85 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    int RQ[100], i, j, n, TotalHeadMoment = 0, initial, size, move, index, temp;
+int head, a[20], i, distance, n, seektime = 0, size;
 
-    // Input: Number of disk requests
-    printf("Enter the number of requests: ");
-    scanf("%d", &n);
-
-    // Input: Disk request queue
-    printf("Enter the request queue: ");
-    for(i = 0; i < n; i++)
-        scanf("%d", &RQ[i]);
-
-    // Input: Initial head position
-    printf("Enter the initial head position: ");
-    scanf("%d", &initial);
-
-    // Input: Total disk size
-    printf("Enter the total disk size: ");
-    scanf("%d", &size);
-
-    // Input: Direction of movement (0 for left, 1 for right)
-    printf("Enter the direction (0 for left, 1 for right): ");
-    scanf("%d", &move);
-
-    // Sorting the request queue in ascending order
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(RQ[j] > RQ[j + 1]) {
-                temp = RQ[j];
-                RQ[j] = RQ[j + 1];
-                RQ[j + 1] = temp;
+void bubbleSort(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
+}
 
-    // Finding the index of the first request greater than the initial head position
-    for(i = 0; i < n; i++) {
-        if(initial < RQ[i]) {
-            index = i;
+int main() {
+    printf("\nEnter Head position: ");
+    scanf("%d", &head);
+
+    printf("Enter number of disk requests: ");
+    scanf("%d", &n);
+
+    printf("Enter the disk size: ");
+    scanf("%d", &size);
+
+    printf("Enter the disk requests: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Add head position to request queue
+    a[n] = head;
+    n++;
+
+    // Sort the requests including head
+    bubbleSort(a, n);
+
+    // Find position of head after sorting
+    int pos;
+    for (i = 0; i < n; i++) {
+        if (a[i] == head) {
+            pos = i;
             break;
         }
     }
 
-    if(move == 1) { // Moving towards higher track numbers (right direction)
-        for(i = index; i < n; i++) {
-            TotalHeadMoment += abs(RQ[i] - initial);
-            initial = RQ[i];
-        }
-        // Moving to the end of the disk
-        TotalHeadMoment += abs(size - RQ[i - 1] - 1);
-        // Jumping to the start (circular)
-        TotalHeadMoment += abs(size - 1 - 0);
-        initial = 0;
+    printf("\n\tC-SCAN DISK SCHEDULING\n\n");
 
-        // Servicing the remaining requests
-        for(i = 0; i < index; i++) {
-            TotalHeadMoment += abs(RQ[i] - initial);
-            initial = RQ[i];
-        }
-    } else { // Moving towards lower track numbers (left direction)
-        for(i = index - 1; i >= 0; i--) {
-            TotalHeadMoment += abs(RQ[i] - initial);
-            initial = RQ[i];
-        }
-        // Moving to track 0
-        TotalHeadMoment += abs(RQ[i + 1] - 0);
-        // Jumping to the highest track
-        TotalHeadMoment += abs(size - 1 - 0);
-        initial = size - 1;
-
-        // Servicing the remaining requests
-        for(i = n - 1; i >= index; i--) {
-            TotalHeadMoment += abs(RQ[i] - initial);
-            initial = RQ[i];
-        }
+    // Move from head towards end
+    for (i = pos; i < n - 1; i++) {
+        distance = a[i + 1] - a[i];
+        printf("Head movement from %d to %d : %d\n", a[i], a[i + 1], distance);
+        seektime += distance;
     }
 
-    // Output the total head movement
-    printf("Total head movement is %d\n", TotalHeadMoment);
-    
+    // Jump to end if not already there
+    if (a[n - 1] != size - 1) {
+        distance = (size - 1) - a[n - 1];
+        printf("Head movement from %d to %d : %d\n", a[n - 1], size - 1, distance);
+        seektime += distance;
+    }
+
+    // Jump from end to start
+    distance = size - 1;
+    printf("Head movement from %d to 0 : %d\n", size - 1, distance);
+    seektime += distance;
+
+    // Continue from start to head's original position
+    for (i = 0; i < pos; i++) {
+        if (i == 0) {
+            distance = a[i];
+            printf("Head movement from %d to %d : %d\n", size - 1, a[i], distance);
+        } else {
+            distance = a[i] - a[i - 1];
+            printf("Head movement from %d to %d : %d\n", a[i - 1], a[i], distance);
+        }
+        seektime += distance;
+    }
+
+    printf("\nTotal seek time is : %d\n", seektime);
     return 0;
 }
